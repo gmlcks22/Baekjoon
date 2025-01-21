137d77ac4d3b3b07f1860be4fe301946f9463429
gmlcks <159685948+gmlcks22@users.noreply.github.com>
2025-01-21 20:49:12 +0900

    [Bronze V] Title: A+B - 7, Time: 132 ms, Memory: 16024 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.java
@@ -0,0 +1,24 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int repeat = Integer.parseInt(br.readLine());
+        int[] result = new int[repeat];
+
+        for(int i=0; i<repeat; i++){
+            String line = br.readLine();
+            String[]nums = line.split(" ");
+            result[i] = Integer.valueOf(nums[0]) + Integer.valueOf(nums[1]);
+        }
+        for(int i=0; i<repeat; i++){
+            bw.write("Case #"+(i+1)+": "+result[i] + "\n");
+        }
+
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
