49b0d29493afa21b42abeeede976aceed2bc9a65
gmlcks <159685948+gmlcks22@users.noreply.github.com>
2025-01-22 20:16:41 +0900

    Update 아스키 코드.java

--- 백준/Bronze/11654. 아스키 코드/아스키 코드.java
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -1,12 +1,9 @@
-import java.io.*;
 import java.util.Scanner;
 
 public class Main{
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-        char word = scanner.next().charAt(0);
-
-        System.out.println((int)word);
+        char word = scanner.next().charAt(0); // chatAt(0): 0번째 글짜 char 형식으로 리턴
+        System.out.println((int)word); // int 형식으로 형변환 --> ascii code
     }
 }
