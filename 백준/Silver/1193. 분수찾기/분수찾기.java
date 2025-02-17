f9a36c4c5ddf0f432b1a216c0d14904b2db5fcfb
gmlcks <159685948+gmlcks22@users.noreply.github.com>
2025-02-18 05:19:32 +0900

    [Silver V] Title: 분수찾기, Time: 192 ms, Memory: 18464 KB -BaekjoonHub

--- 백준/Silver/1193. 분수찾기/분수찾기.java
+++ 백준/Silver/1193. 분수찾기/분수찾기.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Main{
+public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int x = scanner.nextInt();
@@ -9,20 +9,22 @@ public class Main{
         int denominator = 1; // 분모
 
         // x가 속한 열(k) 찾기
-        int k = 1;
-        while(x>k*(k+1)/2){
-            k++;
-        }
+//        int k = 1;
+//        while (x > k * (k + 1) / 2) {
+//            k++;
+//        }
+
+        // x가 속한 열(k) 찾기
+        int k = (int)Math.ceil((-1 + Math.sqrt(1 + 8 * (long) x)) / 2);
 
         // x번째 분수 찾기
-        int nInTheColumn = k - (k*(k+1)/2 - x); // x가 k열의 몇 번째 숫자인지 저장
-        if(k % 2 != 0){ // 1 3 5 7 9
-            numerator = k+1 - nInTheColumn;
+        int nInTheColumn = k - (k * (k + 1) / 2 - x); // x가 k열의 몇 번째 숫자인지 저장
+        if (k % 2 != 0) { // 1 3 5 7 9
+            numerator = k + 1 - nInTheColumn;
             denominator = nInTheColumn;
-        }
-        else{ // 2 4 6 8
+        } else { // 2 4 6 8
             numerator = nInTheColumn;
-            denominator = k+1 - nInTheColumn;
+            denominator = k + 1 - nInTheColumn;
         }
 
         System.out.print(numerator + "/" + denominator);
