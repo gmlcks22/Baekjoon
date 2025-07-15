c68aa79c6814c9fcaeeac79e5134334dd384289e
gmlcks <159685948+gmlcks22@users.noreply.github.com>
2025-07-15 17:34:36 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 5, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24266. 알고리즘 수업 － 알고리즘의 수행 시간 5/알고리즘 수업 － 알고리즘의 수행 시간 5.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	long long n;
+	
+	// The MenOfPassion algorithm has a time complexity of O(n^3).
+	cin >> n;
+	cout << n * n * n << '\n' << 3;
+	return 0;
+}
